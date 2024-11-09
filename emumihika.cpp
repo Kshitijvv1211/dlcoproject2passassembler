0da7cfe9f9ac14c9eada998a968488732696f366
Kshitijvv1211 <145251990+Kshitijvv1211@users.noreply.github.com>
2024-11-09 22:24:57 +0530

    Update emumihika.cpp

--- emumihika.cpp
+++ emumihika.cpp
@@ -54,50 +54,83 @@ void (*instructionFunctions[])(int) = {load_constant, add_constant, load_local, 
                                        adjust_stack_pointer, accumulatorToSP, spToAccumulator, call_procedure,
                                        returnProcedure, branchIfZero, branchIfLessZero, branch};
 
+
 string decimal_to_hexadecimal_conversion(unsigned int num) {
-    stringstream ss;
-    ss << hex << num;
-    string hexRepresentation = ss.str();
-    reverse(hexRepresentation.begin(), hexRepresentation.end());
-    while ((int)hexRepresentation.size() < 8) {
-        hexRepresentation += '0';
+    const char hexDigits[] = "0123456789abcdef";
+    string hexRepresentation(8, '0');  // Start with a string of 8 '0's
+
+    for (int i = 7; i >= 0; --i) {           // Fill the string from right to left
+        hexRepresentation[i] = hexDigits[num % 16];  // Get the last hex digit
+        num /= 16;                          // Move to the next hex digit
     }
-    reverse(hexRepresentation.begin(), hexRepresentation.end());
+
     return hexRepresentation;
 }
 
 void loadMachineCode() {
+    cout << "Enter file name (e.g., machineCode.o): ";
     string fileName;
-    cout << "Enter file name: Ex: machineCode.o" << endl;
-    cin >> fileName;
-    ifstream inputFile(fileName, ios::in | ios::binary);
+    getline(cin, fileName);  // Use getline to handle spaces
+
+    ifstream inputFile(fileName, ios::binary);
+    if (!inputFile) {
+        cerr << "Error: Unable to open file " << fileName << endl;
+        return;
+    }
+
     unsigned int instruction;
-    int pos = 0;
-    while (inputFile.read((char*)&instruction, sizeof(int))) {
-        main_memory[pos++] = instruction;
-        machine_code_lines.push_back(decimal_to_hexadecimal_conversion(instruction));
+    int position = 0;
+
+    while (inputFile.read(reinterpret_cast<char*>(&instruction), sizeof(instruction))) {
+        if (position < sizeof(main_memory) / sizeof(main_memory[0])) {
+            main_memory[position++] = instruction;
+            machine_code_lines.emplace_back(decimal_to_hexadecimal_conversion(instruction));
+        } else {
+            cerr << "Error: Exceeded memory limits." << endl;
+            break;
+        }
+    }
+
+    if (inputFile.eof()) {
+        cout << "Machine code loaded successfully from " << fileName << endl;
+    } else {
+        cerr << "Error: Reading interrupted before reaching end of file." << endl;
     }
 }
 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
 void displayWelcomeMessage() {
-    cout << "Welcome to Emulator" << endl;
-    cout << "Following functions are implemented:" << endl;
-    cout << "1. Memory Dump using instruction: -dump" << endl;
-    cout << "2. Emulate code one line at time: using instruction: -t" << endl;
-    cout << "3. Emulate till the end using -run" << endl;
-    cout << "4. Show registers and SP values: -reg" << endl;
-    cout << "5. Show instruction set using: -isa" << endl;
-    cout << "6. Read operations using -read" << endl;
-    cout << "7. Write operations using -write" << endl;
+    cout << "Welcome to the Emulator!" << endl;
+    cout << "\nAvailable Commands:\n" << endl;
+
+    cout << left << setw(7) << "Sr.No." << setw(10) << "Command" << setw(40) << "Description" << endl;
+    cout << "---------------------------------------------------------------" << endl;
+    cout << left << setw(7) << "1." << setw(10) << "-dump"    << setw(40) << "Display a memory dump" << endl;
+    cout << left << setw(7) << "2." << setw(10) << "-t"       << setw(40) << "Step through code one line at a time" << endl;
+    cout << left << setw(7) << "3." << setw(10) << "-run"     << setw(40) << "Run the code until completion" << endl;
+    cout << left << setw(7) << "4." << setw(10) << "-reg"     << setw(40) << "Display register and stack pointer (SP) values" << endl;
+    cout << left << setw(7) << "5." << setw(10) << "-isa"     << setw(40) << "Show the instruction set" << endl;
+    cout << left << setw(7) << "6." << setw(10) << "-read"    << setw(40) << "Read from memory" << endl;
+    cout << left << setw(7) << "7." << setw(10) << "-write"   << setw(40) << "Write to memory" << endl;
+
+    cout << "\nEnter a command to proceed." << endl;
 }
 
+
 void displayMemoryDump() {
-    for (int i = 0; i < (int)machine_code_lines.size(); i++) {
-        cout << decimal_to_hexadecimal_conversion(i) << " ";
-        for (int j = i; j < min(i + 4, (int)machine_code_lines.size()); j++) {
-            cout << decimal_to_hexadecimal_conversion(main_memory[j]) << " ";
+    int memorySize = static_cast<int>(machine_code_lines.size());
+
+    for (int i = 0; i < memorySize; i += 4) {
+        cout << decimal_to_hexadecimal_conversion(i) << ": ";  // Print memory address in hex
+
+        // Display up to 4 consecutive memory contents at this address
+        for (int j = i; j < i + 4 && j < memorySize; j++) {
+            cout << machine_code_lines[j] << " ";
         }
-        i = i + 3;
+        
         cout << endl;
     }
 }
@@ -117,26 +150,35 @@ void displayWriteOperation() {
          << " to " << decimal_to_hexadecimal_conversion(memory_change[1]) << endl;
 }
 
+
 void displayInstructionSet() {
-    cout << "OpMachineCode Mnemonic Operand" << '\n';
-    cout << "0      ldc      value " << endl;
-    cout << "1      adc      value " << endl;
-    cout << "2      ldl      value " << endl;
-    cout << "3      stl      value " << endl;
-    cout << "4      ldnl     value " << endl;
-    cout << "5      stnl     value " << endl;
-    cout << "6      add            " << endl;
-    cout << "7      sub            " << endl;
-    cout << "9      shr            " << endl;
-    cout << "10     adj      value " << endl;
-    cout << "11     a2sp           " << endl;
-    cout << "12     sp2a           " << endl;
-    cout << "13     call     offset" << endl;
-    cout << "14     return         " << endl;
-    cout << "15     brz      offset" << endl;
-    cout << "16     brlz     offset" << endl;
-    cout << "17     br       offset" << endl;
-    cout << "18     HALT           " << endl;
+    cout << "Instruction Set:\n" << endl;
+
+    cout << left << setw(10) << "Sr. No."
+         << setw(18) << "| OpMachineCode"
+         << setw(20) << "| Mnemonic"
+         << "Operand" << endl;
+
+    cout << "----------------------------------------------------------" << endl;
+
+    cout << left << setw(10) << "0" << setw(18) << "| ldc"   << setw(20) << "| value" << endl;
+    cout << left << setw(10) << "1" << setw(18) << "| adc"   << setw(20) << "| value" << endl;
+    cout << left << setw(10) << "2" << setw(18) << "| ldl"   << setw(20) << "| value" << endl;
+    cout << left << setw(10) << "3" << setw(18) << "| stl"   << setw(20) << "| value" << endl;
+    cout << left << setw(10) << "4" << setw(18) << "| ldnl"  << setw(20) << "| value" << endl;
+    cout << left << setw(10) << "5" << setw(18) << "| stnl"  << setw(20) << "| value" << endl;
+    cout << left << setw(10) << "6" << setw(18) << "| add"   << setw(20) << "|" << endl;
+    cout << left << setw(10) << "7" << setw(18) << "| sub"   << setw(20) << "|" << endl;
+    cout << left << setw(10) << "9" << setw(18) << "| shr"   << setw(20) << "|" << endl;
+    cout << left << setw(10) << "10" << setw(18) << "| adj"   << setw(20) << "| value" << endl;
+    cout << left << setw(10) << "11" << setw(18) << "| a2sp"  << setw(20) << "|" << endl;
+    cout << left << setw(10) << "12" << setw(18) << "| sp2a"  << setw(20) << "|" << endl;
+    cout << left << setw(10) << "13" << setw(18) << "| call"  << setw(20) << "| offset" << endl;
+    cout << left << setw(10) << "14" << setw(18) << "| return" << setw(20) << "|" << endl;
+    cout << left << setw(10) << "15" << setw(18) << "| brz"   << setw(20) << "| offset" << endl;
+    cout << left << setw(10) << "16" << setw(18) << "| brlz"  << setw(20) << "| offset" << endl;
+    cout << left << setw(10) << "17" << setw(18) << "| br"    << setw(20) << "| offset" << endl;
+    cout << left << setw(10) << "18" << setw(18) << "| HALT"  << setw(20) << "|" << endl;
 }
 
 bool executeInstructions(int operation, int maxExecutions = (1 << 25)) {
